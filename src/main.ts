6b2a063446edde082a6d050c26c33570c0b871af
hermaeusmora <aleshka.dev@gmail.com>
2026-07-04 15:34:23 +0300

    feat: setupd class validator and class transformer

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
 
   const PORT = config.getOrThrow<string>('APP_PORT');
 
