30e9dba2eb7841b525fa3ee314436e73a4171cb8
hermaeusmora <aleshka.dev@gmail.com>
2026-07-04 20:41:00 +0300

    feat: add  auth module with Passport, GitHub OAuth, and JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { TodoModule } from './todo/todo.module';
     PrismaModule,
     UserModule,
     TodoModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
